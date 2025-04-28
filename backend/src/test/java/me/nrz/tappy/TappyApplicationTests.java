a3069e4837bac19871a9522378ef0ca62871bff7
loophyun <dhkim960511@naver.com>
2025-04-28 12:37:23 +0900

    init: init prject

---
+++ backend/src/test/java/me/nrz/tappy/TappyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nrz.tappy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TappyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
