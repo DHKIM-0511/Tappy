b2fbc59d89d49797384703c96ed443c963c667e5
loophyun <dhkim960511@naver.com>
2025-05-09 17:15:33 +0900

    fix: Add composite unique constraint to Follow

--- backend/src/main/java/me/nrz/tappy/follow/entity/Follow.java
+++ backend/src/main/java/me/nrz/tappy/follow/entity/Follow.java
@@ -7,6 +7,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,17 +17,20 @@ import me.nrz.tappy.user.entity.User;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"from_id", "to_id"})
+})
 public class Follow {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn
+    @JoinColumn(name = "from_id")
     private User from;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn
+    @JoinColumn(name = "to_id")
     private User to;
 
     private boolean isBreak;
@@ -33,5 +38,13 @@ public class Follow {
     public Follow(User fromUser, User toUser){
         this.from = fromUser;
         this.to = toUser;
+    }
+
+    public void breakFollow(){
+        this.isBreak = true;
+    }
+
+    public void restoreFollow(){
+        this.isBreak = false;
     }
 }
