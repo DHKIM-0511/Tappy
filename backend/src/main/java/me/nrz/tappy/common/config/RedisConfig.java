f0e2745ccb12ca2e44f24b45091c18d14be0f53d
loophyun <dhkim960511@naver.com>
2025-05-13 11:33:21 +0900

    feat: add redis pub sub service

--- backend/src/main/java/me/nrz/tappy/common/config/RedisConfig.java
+++ backend/src/main/java/me/nrz/tappy/common/config/RedisConfig.java
@@ -1,13 +1,30 @@
 package me.nrz.tappy.common.config;
 
+import me.nrz.tappy.room.service.RoomPubSubService;
+import me.nrz.tappy.room.service.RoomPubSubServiceImpl;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+    @Value("${spring.redis.password}")
+    private String password;
+
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
@@ -16,5 +33,37 @@ public class RedisConfig {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         return template;
+    }
+
+    @Bean
+    @Qualifier("redis")
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        configuration.setPassword(password);
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    @Qualifier("redis")
+    public StringRedisTemplate stringRedisTemplate(@Qualifier("redis") RedisConnectionFactory redisConnectionFactory){
+        return new StringRedisTemplate(redisConnectionFactory);
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+        @Qualifier("redis") RedisConnectionFactory redisConnectionFactory,
+        @Qualifier("roomMessageListenerAdapter") MessageListenerAdapter roomMessageListenerAdapter
+    ){
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(roomMessageListenerAdapter, new PatternTopic("room/*"));
+        return container;
+    }
+    @Bean
+    public MessageListenerAdapter roomMessageListenerAdapter(
+        RoomPubSubService roomPubSubService){
+        return new MessageListenerAdapter(roomPubSubService, "onMessage");
     }
 }
